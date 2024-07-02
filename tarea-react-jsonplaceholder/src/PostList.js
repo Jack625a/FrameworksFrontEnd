3a1e3f705ee7a91e3056dcf1adbc4bcbd5cc9efe
jimmy <naither@gmail.com>
2024-07-01 22:34:03 -0400

    tarea react

---
+++ tarea-react-jsonplaceholder/src/PostList.js
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from 'react';
+
+const PostList = () => {
+  const [posts, setPosts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const fetchPosts = async () => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+      if (!response.ok) {
+        throw new Error('Error al obtener las publicaciones');
+      }
+      const data = await response.json();
+      setPosts(data);
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  if (isLoading) {
+    return <p>Cargando...</p>;
+  }
+
+  if (error) {
+    return <p>Error: {error}</p>;
+  }
+
+  return (
+    <div>
+      <h1>Lista de Publicaciones</h1>
+      <button onClick={fetchPosts}>Actualizar Publicaciones</button>
+      <ul>
+        {posts.map(post => (
+          <li key={post.id}>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default PostList;
