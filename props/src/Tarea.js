dbb967ef4065b8ee84c16d2889564a054743f067
jimmy <naither@gmail.com>
2024-07-05 22:13:57 -0400

    react

--- props/src/Tarea.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-function Tarea({titulo,completada}){
-    return(
-        <div className='tarea' style={{textDecoration:completada? 'line-through':'none'}}>
-            {titulo}
-        </div>
-    );
-}
-export default Tarea;
